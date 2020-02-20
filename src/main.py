41972e7de015c86b67d9a30f817cb2e09266796f
DarkWelcome <kevin.bienvenu@battlemythe.net>
2020-02-20 19:55:54 +0100

    first push

--- src/main.py
+++ src/main.py
@@ -1,27 +1,35 @@
 from src import parser, submit
 
+list_file = [
+    "a_example.txt",
+    "b_read_on.txt",
+    "c_incunabula.txt",
+    "d_tough_choices.txt",
+    "e_so_many_books.txt",
+    "f_libraries_of_the_world.txt"
+]
+
 if __name__ == "__main__":
-    books, libraries = parser.parse("a_example.txt")
 
-    result = []
+    for file in list_file:
+        books, libraries = parser.parse(file)
 
-    for l in libraries.values():
-        l.compute_score(books)
-        print(l.score)
+        result = []
 
-    array_lib = list(libraries.values())
-    array_lib.sort(key=lambda l: l.score, reverse=True)
+        for l in libraries.values():
+            l.compute_score(books)
 
-    for l in array_lib:
-        result.append({
-            "id_library": l.id,
-            "books": [books[b] for b in l.books]
-        })
-        result[-1]["books"].sort(key=lambda b: b.score, reverse=True)
-        result[-1]["books"] = [b.id for b in result[-1]["books"]]
+        array_lib = list(libraries.values())
+        array_lib.sort(key=lambda l: l.score, reverse=True)
 
-    print(result)
+        for l in array_lib:
+            result.append({
+                "id_library": l.id,
+                "books": [books[b] for b in l.books]
+            })
+            result[-1]["books"].sort(key=lambda b: b.score, reverse=True)
+            result[-1]["books"] = [b.id for b in result[-1]["books"]]
 
-    submission = submit.Submission(result)
+        submission = submit.Submission(result)
 
-    submission.submit('data/submission.txt')
+        submission.submit('data/sub_' + file)
